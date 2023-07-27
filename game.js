7153e6c419f70dee14a74bb4ea84f8c4217f8dc9
Susan Segura <susansegura26@gmail.com>
2023-07-27 16:45:17 -0600

    Initial commit

---
+++ game.js
@@ -0,0 +1,112 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var level = 0;
+
+var started = false;
+
+$(document).on("keydown", function(event) {
+  if (started === false) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+
+})
+
+
+//WHEN USER CLICKS A BUTTON...
+$(".btn").on("click", function() {
+
+  var userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+
+  var userLastAnswer = userClickedPattern.length - 1;
+  checkAnswer(userLastAnswer);
+
+});
+
+
+
+function nextSequence() {
+  //RANDOM GAME PATTERN...
+  userClickedPattern = [];
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+
+  level++;
+  $("#level-title").text("Level " + level);
+
+}
+
+//currentLevel = index (ex; 0, x , string)
+function checkAnswer(currentLevel) {
+  console.log('currentLevel: ', currentLevel);
+  console.log('userClickedPattern[currentLevel]: ', userClickedPattern[currentLevel]);
+  console.log('gamePattern: ', gamePattern);
+  console.log('userClickedPattern: ', userClickedPattern);
+
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+
+    }
+  } else {
+
+    console.log("wrong");
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+
+// RESET THE GAME
+function startOver() {
+  level = 0;
+  started = false;
+  gamePattern = [];
+}
+
+
+// PLAY AUDIO (USED FOR BOTH RANDOM PATTERN AND BUTTON CLICKED)
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//ANIMATION FOR CLICKED BUTTON
+function animatePress(currentColour) {
+  $("." + currentColour).addClass("pressed");
+
+  setTimeout(function() {
+    $("." + currentColour).removeClass("pressed");
+  }, 100);
+}
